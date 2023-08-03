788a1966141d0d6cecaa048c5e8a39902fc67e96
adpatter <adpatter@umich.edu>
2023-08-02 20:02:59 -0400

    Fix test.

---
+++ test/src/index.ts
@@ -0,0 +1,37 @@
+import { Worker, isMainThread, parentPort } from 'node:worker_threads';
+import { fileURLToPath } from 'node:url';
+import { Agent } from 'port_agent';
+
+if (isMainThread) { // This is the Main Thread.
+    (async () => {
+        const worker = new Worker(fileURLToPath(import.meta.url));
+        const agent = new Agent(worker);
+        worker.on('online', async () => {
+            try {
+                let greeting = await agent.call('hello_world', 'again, another');
+                console.log(greeting);
+                await agent.call('error', 'again, another');
+            }
+            catch (err) {
+                console.error(err);
+            }
+            finally {
+                worker.terminate();
+            }
+        });
+        let greeting = await agent.call<string>('hello_world', 'another');
+        console.log(greeting);
+    })();
+} else { // This is a Worker Thread.
+    function nowThrowAnError() {
+        throw new Error('To err is Human.');
+    }
+    function callAFunction() {
+        nowThrowAnError();
+    }
+    if (parentPort) {
+        const agent = new Agent(parentPort);
+        agent.register('hello_world', (value: string) => `Hello ${value} world!`);
+        agent.register('error', callAFunction);
+    }
+} 
