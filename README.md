d4603e0a2b4863103da26e28822666e662c78142
adpatter <adpatter@umich.edu>
2023-09-30 13:40:20 -0400

    Improve example.

--- README.md
+++ README.md
@@ -89,9 +89,54 @@ const greeting = await agent.call<string>('hello_world', 'happy');
 
 ## Examples
 
-### An Example
+### A Simple Example
 
 In this example you will:
+
+1. Instantiate a worker thread.
+2. Instantiate an `Agent` in the main thread.
+3. Use the `Agent` to call the `hello_world` function.
+4. Instantiate an `Agent` in the worker thread.
+5. Use the `Agent` in order to register a function to handle calls to the `hello_world` function.
+6. Resolve (3) and log the result to the console.
+
+`examples/simple/index.js`
+```js
+import { Worker, isMainThread, parentPort } from 'node:worker_threads';
+import { fileURLToPath } from 'node:url';
+import { Agent } from 'port_agent';
+
+if (isMainThread) { // This is the main thread.
+    void (async () => {
+        const worker = new Worker(fileURLToPath(import.meta.url)); // (1)
+        const agent = new Agent(worker); // (2)
+        try {
+            const greeting = await agent.call('hello_world', 'another'); // (3)
+            console.log(greeting); // (6)
+        }
+        catch (err) {
+            console.error(err);
+        }
+        finally {
+            worker.terminate();
+        }
+    })();
+}
+else { // This is a worker thread.
+    if (parentPort) {
+        const agent = new Agent(parentPort); // (4)
+        agent.register('hello_world', (value) => `Hello, ${value} world!`); // (5)
+    }
+}
+```
+The example should log to the console:
+
+```bash
+Hello, another world!
+```
+### A Test
+
+In this test you will:
 
 1. Instantiate a worker thread.
 2. Instantiate an Agent in the main thread.
@@ -202,7 +247,7 @@ if (isMainThread) { // This is the main thread.
 } 
 ```
 
-This example should log to the console something that looks similar to this:
+This test should log to the console something that looks similar to this:
 
 ```bash
 Hello, another world!
--- examples/simple/index.js
+++ examples/simple/index.js
@@ -1,4 +1,4 @@
-import { Worker, isMainThread, parentPort, threadId } from 'node:worker_threads';
+import { Worker, isMainThread, parentPort } from 'node:worker_threads';
 import { fileURLToPath } from 'node:url';
 import { Agent } from 'port_agent';
 
@@ -6,59 +6,21 @@ if (isMainThread) { // This is the main thread.
     void (async () => {
         const worker = new Worker(fileURLToPath(import.meta.url)); // (1)
         const agent = new Agent(worker); // (2)
-        // eslint-disable-next-line @typescript-eslint/no-misused-promises
-        worker.on('online', /*(4)*/ async () => {
-            try {
-                const greeting = await agent.call('hello_world', 'again, another'); // (9)
-                console.log(greeting); // (11)
-                await agent.call('a_reasonable_assertion', 'To err is Human.'); // (12)
-            }
-            catch (err) {
-                console.error(`Now, back in the main thread, we will handle the`, err); // (13)
-            }
-            finally {
-                void worker.terminate(); // (14)
-                // eslint-disable-next-line @typescript-eslint/no-misused-promises
-                setTimeout(async () => {
-                    try {
-                        await agent.call('hello_world', 'no more...'); // (15)
-                    }
-                    catch (err) {
-                        if (err instanceof Error) {
-                            console.error(err);
-                        }
-                        else if (typeof err == 'number') {
-                            console.log(`Exit code: ${err.toString()}`); // (16)
-                        }
-                    }
-                    // I wouldn't call this magic, but it's worth considering nonetheless; this is a *very* late binding registrant.
-                    agent.register('magic', (value) => console.log(`Seriously, the worker's thread ID was ${value}.`)); // (17)
-                }, 4);
-            }
-        });
         try {
-            // This call will be invoked once the `hello_world` function has been bound in the worker.
             const greeting = await agent.call('hello_world', 'another'); // (3)
-            console.log(greeting); // (10)
+            console.log(greeting); // (6)
         }
         catch (err) {
             console.error(err);
         }
+        finally {
+            worker.terminate();
+        }
     })();
 }
 else { // This is a worker thread.
-    function nowThrowAnError(message) {
-        // This seems reasonable...
-        assert.notEqual(typeof new Object(), typeof null, message);
-    }
-    function callAFunction(message) {
-        nowThrowAnError(message);
-    }
     if (parentPort) {
-        const agent = new Agent(parentPort); // (5)
-        agent.register('hello_world', (value) => `Hello, ${value} world!`); // (6)
-        // This will throw in the main thread.
-        agent.register('a_reasonable_assertion', callAFunction); // (7).
-        const result = await agent.call('magic', threadId); // (8)
+        const agent = new Agent(parentPort); // (4)
+        agent.register('hello_world', (value) => `Hello, ${value} world!`); // (5)
     }
 }
